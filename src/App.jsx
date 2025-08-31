873e7c9bbc8ad96b85dd85d685aa17b4cb594fe6
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-08-31 18:52:26 +0200

    Refactor layout and update styles for responsive design
    Enhanced the main layout with new classes for better responsiveness on medium screens. Updated global styles to include a background color and adjusted height properties. These changes improve the app's appearance and usability on larger devices.

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,10 @@ import Button from "./components/Button.jsx";
 function App() {
 
   return (
-      <main className="h-screen">
-          <section className="card">
+      <main className="bg-[#36384D] md:flex md:flex-col md:items-center md:px-20 md:py-[73px]">
+          <section className="card md:p-10 bg-white rounded-3xl">
             <Illustration />
-          </section>
-          <section className="flex flex-col justify-center gap-10 px-[24px] py-10">
+          <section className="flex flex-col justify-center gap-10 px-[24px] py-10 md:gap-6 md:px-0 md:pb-0">
               <div className="flex flex-col gap-6">
                 <h1 className="text-preset-1">Stay updated!</h1>
                   <p className="text-preset-2-regular">Join 60,000+ product managers receiving monthly updates on:</p>
@@ -21,6 +20,7 @@ function App() {
                   <TextInput name="email" label="Email address" placeholder="email@company.com"  />
                   <Button text='Subscribe to monthly newsletter' variant='primary'/>
               </form>
+          </section>
           </section>
       </main>
   )
